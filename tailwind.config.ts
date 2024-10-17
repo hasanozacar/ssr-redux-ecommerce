98bddd2e39906d18cd65bc62a2e16d5b842a24f8
= <Hasan.Ozacar@agesa.com.tr>
2024-10-17 15:52:13 +0300

    added tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{js,ts,jsx,tsx}',  
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#1a202c', 
+        secondary: '#2d3748',
+        accent: '#38b2ac',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      spacing: {
+        '128': '32rem',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
