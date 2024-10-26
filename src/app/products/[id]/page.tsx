f485a85af4b9f1eba3344ac588769c9cde11e273
= <Hasan.Ozacar@agesa.com.tr>
2024-10-26 15:16:33 +0300

    Cross-Site Scripting (XSS) with DOMPurify

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { useSelector } from "react-redux";
 import { selectAllProducts } from "@/store/slices/products";
 import { useParams, useRouter } from "next/navigation";
+import DOMPurify from "dompurify";
 
 export default function ProductDetail() {
   const params = useParams();
@@ -13,6 +14,8 @@ export default function ProductDetail() {
   if (!product) {
     return <div className="text-center text-red-500">Ürün bulunamadı</div>;
   }
+
+  const sanitizedDescription = DOMPurify.sanitize(product.description);
 
   return (
     <div className="container mx-auto p-6 bg-white shadow-md rounded-lg">
@@ -26,9 +29,9 @@ export default function ProductDetail() {
           Rating: {product.rating.rate} ({product.rating.count} oy)
         </p>
       </div>
-      <p className="text-gray-700 leading-relaxed text-center">{product.description}</p>
+
+      <div className="text-gray-700 leading-relaxed text-center" dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
       <div className="mt-6 flex justify-center space-x-4">
-        {" "}
         <button
           onClick={() => router.back()}
           className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-lg hover:bg-gray-400 transition duration-300"
