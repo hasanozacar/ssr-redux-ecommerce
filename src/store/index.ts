56f62d55a639dce2e50b2a887561e94594a45e77
= <Hasan.Ozacar@agesa.com.tr>
2024-10-28 08:16:18 +0300

    Added Middleware and Logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,24 @@
-'use client';
+"use client";
 
-import { configureStore } from '@reduxjs/toolkit';
-import productsReducer from './slices/products';
+import { configureStore } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
+import productsReducer from "./slices/products";
+import { Middleware } from "redux";
+
+// Logger middleware oluştur
+const logger = createLogger();
+
+// Example Middleware
+const apiMiddleware: Middleware = (store) => (next) => (action) => {
+  console.log("API action dispatched:", action);
+  return next(action); // Action'ı bir sonraki middleware'e geç
+};
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger,apiMiddleware), // Middleware'leri ekle
 });
 
 export type RootState = ReturnType<typeof store.getState>;
